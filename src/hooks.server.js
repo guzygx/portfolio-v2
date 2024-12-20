250c83ff9d9eac5994c159d3b79507b1641e0e4d
Guzy GX <gxuzxu@gmail.com>
2024-12-20 15:39:06 +0100

    fix build issue

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,11 +1,13 @@
+import { sequence } from '@sveltejs/kit/hooks';
 import { i18n } from '$lib/i18n';
 const handleParaglide = i18n.handle();
-export const handle = handleParaglide;
 
 // Fix some content-type error during initial hydratation
-export function handle({ event, resolve }) {
-	return resolve(event, {
-		// you can also seralize other headers here if needed
-		filterSerializedResponseHeaders: (name, value) => name === 'content-type'
-	});
+const contentTypeResolver = async ({ event, resolve }) => {
+  return resolve(event, {
+    // you can also seralize other headers here if needed
+    filterSerializedResponseHeaders: (name, value) => name === 'content-type'
+  });
 }
+
+export const handle = sequence(contentTypeResolver, handleParaglide);
