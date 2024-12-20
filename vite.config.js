e7df92a6b925b67d1cf85b04b2633e6c5c848b30
Guzy GX <gxuzxu@gmail.com>
2024-12-20 15:53:14 +0100

    add base url in config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	base: "https://portfolio-v2-e45r.onrender.com/",
 	plugins: [
 		sveltekit(),
 		paraglide({
