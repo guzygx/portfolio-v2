9f8175cb9876c4098d6998eb1cce6fda5812f4f4
Guzy GX <gxuzxu@gmail.com>
2024-12-20 14:57:29 +0100

    fix initial hydratation

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,11 @@
 import { i18n } from '$lib/i18n';
 const handleParaglide = i18n.handle();
 export const handle = handleParaglide;
+
+// Fix some content-type error during initial hydratation
+export function handle({ event, resolve }) {
+	return resolve(event, {
+		// you can also seralize other headers here if needed
+		filterSerializedResponseHeaders: (name, value) => name === 'content-type'
+	});
+}
